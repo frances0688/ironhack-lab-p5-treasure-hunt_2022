66882234f9a2c636bfcb81fb1007a6247fa5af65
Frances Morales Velilla <frances0688@gmail.com>
2022-10-30 17:36:59 +0100

    Frances Morales-missing collision function & bonus

--- src/game.js
+++ src/game.js
@@ -1,7 +1,62 @@
 class Game {
   drawGrid() {
-    // Iteration 1
-    // Draw the grid
-    // https://p5js.org/reference/#/p5/line
+    for (let i=0; i < HEIGHT/10; i++) {
+     line(0, i*HEIGHT/10, WIDTH, i*HEIGHT/10);
+     line(i*WIDTH/10, 0, i*WIDTH/10, HEIGHT);
+    }
+  }
+
+}
+
+class Player {
+  constructor() {
+    this.col = 0
+    this.row = 0
+    this.image
+    this.imageDown
+    this.imageUp
+    this.imageLeft
+    this.imageRight
+  }
+
+  moveUp() {
+    this.row -= 100;
+  }
+
+  moveDown() {
+    this.row += 100;
+  }
+
+  moveLeft() {
+    this.col -= 100;
+  }
+
+  moveRight() {
+    this.col += 100;
+  }
+
+  draw() {
+    image(this.image, this.col, this.row, 100, 100);
+
   }
 }
+
+class Treasure {
+  constructor () {
+    this.col = 0;
+    this.row = 0;
+    this.image;
+    this.setRadomPosition();
+  }
+
+  setRadomPosition(){
+    this.col = Math.floor(Math.random()*10)*100;
+    this.row = Math.floor(Math.random()*10)*100;
+  }
+
+  drawTreasure() {
+    image(this.image, this.col, this.row, 100, 100);
+  }
+}
+
+
--- src/main.js
+++ src/main.js
@@ -1,10 +1,48 @@
 const game = new Game();
+const player = new Player();
+const treasure = new Treasure();
 
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
+
+  player.image = loadImage('../assets/character-down.png');
+  player.imageDown = loadImage('../assets/character-down.png');
+  player.imageUp = loadImage('../assets/character-up.png');
+  player.imageLeft = loadImage('../assets/character-left.png');
+  player.imageRight = loadImage('../assets/character-right.png');
+
+  treasure.image = loadImage('../assets/treasure.png');
 }
 
 function draw() {
+  
   game.drawGrid();
+  player.draw();
+  treasure.drawTreasure();
+  
 }
+
+function keyPressed() {
+  clear();
+  if (keyCode === 38) {
+    player.moveUp();
+    player.image = player.imageUp;
+  } 
+  
+  if (keyCode === 40) {
+    player.moveDown();
+    player.image = player.imageDown;
+  }
+  
+  if (keyCode === 37) {
+    player.moveLeft();
+    player.image = player.imageLeft;
+  }
+  
+  if (keyCode === 39) {
+    player.moveRight();
+    player.image = player.imageRight;
+  }
+}
+
